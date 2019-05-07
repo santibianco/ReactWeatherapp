0b284494abb45a584946d5f5652864619e2b8de3
Santiago Bianco <santiago.bianco.sb@gmail.com>
2019-05-07 15:02:38 -0300

    state updater

--- weatherapp/src/components/WeatherLocation/index.js
+++ weatherapp/src/components/WeatherLocation/index.js
@@ -9,21 +9,47 @@ import { CLOUD,
     WINDY } from './../../constants/weathers';
 import './style.css';
 
-const data ={
+const data1 ={
     temperature: 20,
     weatherState: CLOUDY,
     humidity: 80,
     wind: '10 m/s',
 }
 
+const data2 ={
+    temperature: 30,
+    weatherState: SUN,
+    humidity: 10,
+    wind: '5 m/s',
+}
+
 class WeatherLocation extends Component {
 
-    render = () => (
-        <div className='weatherLocationCont'>
-            <Location city={"Buenos Aires"} />
-            <WeatherData data={data}/>
-        </div>
-    );
+    constructor(){
+        super();
+        this.state = {
+            city: "Buenos Aires",
+            data: data1,
+        }
+    }
+
+    handleUpdateClick = () => {
+        this.setState({
+            data: data2,
+        });
+    }
+
+    render = () => {
+        const { city, data } = this.state; 
+        
+        return(
+            <div className='weatherLocationCont'>
+                <Location city={city} />
+                <WeatherData data={data}/>
+                <button onClick={this.handleUpdateClick}>Actualizar</button>
+            </div>
+        );
+    };
 
 }
 
