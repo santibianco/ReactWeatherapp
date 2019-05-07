6ef671b697585f4b013296d2f226656b25197a74
Santiago Bianco <santiago.bianco.sb@gmail.com>
2019-05-07 11:48:55 -0300

    adding constants file

--- weatherapp/src/components/WeatherData.js
+++ weatherapp/src/components/WeatherData.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature';
+import { CLOUD, 
+    CLOUDY,
+    RAIN,
+    SUN,
+    SNOW,
+    WINDY } from './../constants/weathers';
 
 const WeatherData = () => (
     <div>
-        <WeatherTemperature temperature={20} weatherState="cloud"/>
+        <WeatherTemperature temperature={20} weatherState={SUN}/>
         <WeatherExtraInfo humidity={80} wind={'10m/s'}/>
     </div>
 );
--- weatherapp/src/components/WeatherTemperature.js
+++ weatherapp/src/components/WeatherTemperature.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import WeatherIcons from 'react-weathericons';
+import { CLOUD, 
+         CLOUDY,
+         RAIN,
+         SUN,
+         SNOW,
+         WINDY } from './../constants/weathers';
+
+const stateToIconName = weatherState => {
+    switch(weatherState){
+        case CLOUD:
+            return "cloud";
+        case CLOUDY:
+            return "cloudy";
+        case SUN:
+            return "day-sunny";
+        case RAIN:
+            return "rain";
+        case SNOW:
+            return "snow";
+        case WINDY:
+            return "windy";    
+        default:
+            return "day-sunny";
+    }
+};
 
 const getWeatherIcon = weatherState => {
-    switch(weatherState){
-        case "cloud":
-            return (<WeatherIcons name="cloud" size="2x"/>)
-        default:
-            return(<WeatherIcons name="sleet" size="2x"/>)
-    }
+    return(<WeatherIcons name={stateToIconName(weatherState)} size="2x"/>)
 };
 
 const WeatherTemperature = ({ temperature, weatherState }) => (
