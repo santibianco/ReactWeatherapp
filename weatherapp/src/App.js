7ed99d6869e7246f2eb5122800ccad10be1ed64f
Santiago Bianco <santiago.bianco.sb@gmail.com>
2019-05-21 17:08:16 -0300

    responsive design

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper'
+import AppBar from 'material-ui/AppBar'
 import LocationList from './components/LocationList';
 import './App.css';
 
@@ -19,12 +22,26 @@ class App extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <div className="App">
-          <LocationList 
-            onSelectedLocation = {this.handleSelectedLocation}
-            cities={cities}
-          />
-        </div>
+        <Grid>
+          <Row>
+            <Col xs={12}>
+              <AppBar title="Weather App" />
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12} md={6}>
+              <LocationList 
+                onSelectedLocation = {this.handleSelectedLocation}
+                cities={cities}
+              />
+            </Col>
+            <Col xs={12} md={6}>
+              <Paper zDepth={4}>
+                <div className='detail'></div>
+              </Paper>
+            </Col>
+          </Row>
+        </Grid>
       </MuiThemeProvider>
     );
   }
--- weatherapp/src/components/LocationList.js
+++ weatherapp/src/components/LocationList.js
@@ -1,7 +1,7 @@
 import React from 'react'; 
 import PropTypes from 'prop-types';
 import WeatherLocation from './WeatherLocation'
-
+import './styles.css'
 
 const LocationList = ( {cities, onSelectedLocation} ) => {
     
@@ -20,7 +20,7 @@ const LocationList = ( {cities, onSelectedLocation} ) => {
                   )
     );        
     
-    return(<div>
+    return(<div className="locationList">
         {stringToComponent(cities)}
     </div>)
 };
